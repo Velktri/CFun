935e44de07b9750d1a8a068ac07c013e80dbc466
Velktri <murrayg226@gmail.com>
2016-01-31 19:37:34 -0800

    boilerplate code

--- OOP_List.c
+++ OOP_List.c
@@ -3,4 +3,20 @@
  *  object oriented approach.
  */
 
+typedef struct {
+  int value;
+  struct node* next;
+} node;
 
+typedef struct {
+  node* root;
+  int (*get)(void *this, int index);
+
+
+} LinkedList;
+
+int main() {
+  LinkedList list;
+
+  return 0;
+}
